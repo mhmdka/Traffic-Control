5360787d3ce09d67a8234b5afd6cc503db8d1ab0
armanghoreshi <armangh1377@gmail.com>
2018-07-08 16:48:45 +0430

    Arman - ManageVehicle

--- Traffic Control/Assets/Scripts/ManageVehicles.cs
+++ Traffic Control/Assets/Scripts/ManageVehicles.cs
@@ -2,15 +2,84 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ManageV : MonoBehaviour {
+public class ManageVehicle : MonoBehaviour
+{
+    //=================== properties ======================//
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    protected bool isElementChanged = false;
+    protected bool isEventOccured = false;
+    protected bool isTrafficChanged = false;
+    protected List<Vehicle> vehicles = new List<Vehicle>();
+
+    //------------------ set/get -------------------------//
+
+    public bool IsElementChanged
+    {
+        set { isElementChanged = value; }
+        get { return isElementChanged; }
+    }
+
+    public bool IsEventOccured
+    {
+        set { isEventOccured = value; }
+        get { return isEventOccured; }
+    }
+
+    public bool IsTrafficChanged
+    {
+        set { isTrafficChanged = value; }
+        get { return isTrafficChanged; }
+    }
+
+    public List<Vehicle> Vehicles
+    {
+        set;
+        get;
+    }
+
+    //====================================== Methods ========================================//
+
+    public void createVehicle(Type typeOfVehicle)
+    {
+
+
+        //------------------ Temp Variables ------------------//
+        Point tmp_point;
+        //----------------------------------------------------//
+
+
+
+        //------------------- Program ------------------------//
+
+        tmp_point = randomPointGenerator();
+        Vehicle tmp_vehicle = new Vehicle(tmp_point, typeOfVehicle);
+
+
+        //----------------------------------------------------//
+
+
+    }
+
+    public void manageMovement(Events events)
+    {
+
+    }
+    private Point randomPointGenerator()
+    {
+
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
