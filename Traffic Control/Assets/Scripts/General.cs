cabbef132078babfb42c9c615c9fb77e4f39fba2
Mohammad Kahani <moh.kahani@gmail.com>
2018-07-09 19:34:49 +0430

    Mohammad - search update

--- Traffic Control/Assets/Scripts/General.cs
+++ Traffic Control/Assets/Scripts/General.cs
@@ -4,9 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class General : Type {
+    protected override int search()
+    {
+        throw new NotImplementedException();
+        return -1;
+    }
 
 
-    
+
 
     // Use this for initialization
     void Start () {
