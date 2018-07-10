0434ebf4949cfbdf07c3d1ed18197794b4e81101
Galiold <goldani.ali@aol.com>
2018-07-10 10:29:37 +0430

    Ali - Analysis Fix

--- Traffic Control/Assets/Scripts/Analysis.cs
+++ Traffic Control/Assets/Scripts/Analysis.cs
@@ -6,6 +6,7 @@ public class Analysis : MonoBehaviour
 {
     public Text analytics;
     private Street[] streetsOnStart;
+
     private void OnEnable()
     {
         //Debug.Log("activated");
@@ -18,7 +19,7 @@ public class Analysis : MonoBehaviour
     {
         for (int i = 0; i < streetsOnStart.Length; i++)
         {
-            if (streetsOnStart[i] != MapClass.Streets[i])
+            if (streetsOnStart[i].TrafficCoef != MapClass.Streets[i].TrafficCoef)
                 analytics.text = analytics.text + "changes on  street " + MapClass.Streets[i].StreetNumber + "has Changed."; 
                 
         }
